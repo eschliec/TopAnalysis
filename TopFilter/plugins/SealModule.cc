7a4c5aceaebdb7109c4bd32821771e62985c3900
mgoerner <>
2010-02-03 09:53:41 +0000

    removed FullLepHypothesesFilter because it doesnt exist

--- TopFilter/plugins/SealModule.cc
+++ TopFilter/plugins/SealModule.cc
@@ -12,5 +12,5 @@ DEFINE_FWK_MODULE(SemiLeptonicTopJetSelector);
 #include "TopAnalysis/TopFilter/plugins/SemiLeptonicTopMuonSelector.h"
 DEFINE_FWK_MODULE(SemiLeptonicTopMuonSelector);
 
-#include "TopAnalysis/TopFilter/plugins/FullLepHypothesesFilter.h"
-DEFINE_FWK_MODULE(FullLepHypothesesFilter);
+//#include "TopAnalysis/TopFilter/plugins/FullLepHypothesesFilter.h"
+//DEFINE_FWK_MODULE(FullLepHypothesesFilter);

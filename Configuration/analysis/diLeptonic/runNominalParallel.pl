8af7246142a749dccb589cbbbe18eba54f094c9c
wbehrenh <>
2012-11-13 18:11:50 +0000

    script to run analysis in parallel

---
+++ Configuration/analysis/diLeptonic/runNominalParallel.pl
@@ -0,0 +1,13 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+
+open my $in, '<', 'selectionList.txt' or die $!;
+chomp(my @files = grep { /\w/ } grep { !/^\s*#/ } <$in>);
+
+while (@files) {
+    my $file = shift @files;
+    system("./load_Analysis -f $file &");
+}
+
---
+++ Configuration/analysis/diLeptonic/runNominalVariations.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+for sys in PU_UP PU_DOWN TRIG_UP TRIG_DOWN BTAG_UP BTAG_DOWN; do
+    ./load_Analysis -s $sys &
+done
+wait
+

a9e0f02a42db2d3ada3459c1862ba8efb3211923
wbehrenh <>
2012-11-15 16:10:39 +0000

    all systematics

--- Configuration/analysis/diLeptonic/mk_HistoFileList.sh
+++ Configuration/analysis/diLeptonic/mk_HistoFileList.sh
@@ -8,8 +8,8 @@ foreach sample (run qcd dyee dymumu dytautau ww wz zz wtolnu single ttbarbg ttba
    foreach channel (ee emu mumu)
      
        
-#       foreach Syst  (Nominal JESUP JESDOWN RESUP RESDOWN PU_UP PU_DOWN SCALEUP SCALEDOWN MATCHUP MATCHDOWN MASSUP MASSDOWN POWHEG MCATNLO HADUP HADDOWN BTAG_UP BTAG_DOWN BTAG_PT_UP BTAG_PT_DOWN BTAG_ETA_UP BTAG_ETA_DOWN)
-    foreach Syst  (Nominal PU_DOWN PU_UP)
+       foreach Syst  (Nominal JESUP JESDOWN RESUP RESDOWN PU_UP PU_DOWN SCALEUP SCALEDOWN MATCHUP MATCHDOWN MASSUP MASSDOWN POWHEG MCATNLO HADUP HADDOWN BTAG_UP BTAG_DOWN BTAG_PT_UP BTAG_PT_DOWN BTAG_ETA_UP BTAG_ETA_DOWN)
+#    foreach Syst  (Nominal PU_DOWN PU_UP)
       
 
       ls -1 selectionRoot/$Syst/$channel/${channel}_$sample*.root >> FileLists/HistoFileList_$Syst\_$channel.txt

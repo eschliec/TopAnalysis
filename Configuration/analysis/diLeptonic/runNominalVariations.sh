dca76a0cc387aa8eadcc1bf860355ccbfdb6c367
wbehrenh <>
2012-11-15 16:11:31 +0000

    btag pt/eta up/down

--- Configuration/analysis/diLeptonic/runNominalVariations.sh
+++ Configuration/analysis/diLeptonic/runNominalVariations.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-for sys in PU_UP PU_DOWN TRIG_UP TRIG_DOWN BTAG_UP BTAG_DOWN; do
+for sys in PU_UP PU_DOWN TRIG_UP TRIG_DOWN BTAG_UP BTAG_DOWN BTAG_PT_UP BTAG_PT_DOWN BTAG_ETA_UP BTAG_ETA_DOWN; do
     ./load_Analysis -s $sys -f Nominal &
 done
 wait

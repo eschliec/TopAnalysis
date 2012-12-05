c4260e7dfe6e531f315e0a1b08cc3f0b2d9297b3
wbehrenh <>
2012-12-05 14:38:53 +0000

    unfolding 3 channels in 10 secs :-) !!!

--- Configuration/analysis/diLeptonic/utils.cpp
+++ Configuration/analysis/diLeptonic/utils.cpp
@@ -1,6 +1,11 @@
 #include "utils.h"
 #include <cmath>
 #include <string>
+#include <iostream>
+#include <stdlib.h>
+#include <TObject.h>
+#include <TPaveText.h>
+#include <type_traits>
 
 void LVtod4(const LV lv, double *d) {
     d[0] = lv.E();
@@ -21,4 +26,305 @@ std::string d2s(double d) {
         sprintf(result, "%.0f", d);
         return std::string(result);
     }
+}
+
+void setHHStyle(TStyle& HHStyle)
+{
+    const int fontstyle=42;
+    HHStyle.SetPalette(1);
+        
+    // ==============
+    //  Canvas
+    // ==============
+            
+    HHStyle.SetCanvasBorderMode(0);
+    HHStyle.SetCanvasColor(kWhite);
+    HHStyle.SetCanvasDefH(600); //Height of canvas
+    HHStyle.SetCanvasDefW(600); //Width of canvas
+    HHStyle.SetCanvasDefX(0);   //Position on screen
+    HHStyle.SetCanvasDefY(0);
+            
+    // ==============
+    //  Pad
+    // ==============
+            
+    HHStyle.SetPadBorderMode(0);
+    // HHStyle.SetPadBorderSize(Width_t size = 1);
+    HHStyle.SetPadColor(kWhite);
+    HHStyle.SetPadGridX(false);
+    HHStyle.SetPadGridY(false);
+    HHStyle.SetGridColor(0);
+    HHStyle.SetGridStyle(3);
+    HHStyle.SetGridWidth(1);
+            
+    // ==============
+    //  Frame
+    // ==============
+            
+    HHStyle.SetFrameBorderMode(0);
+    HHStyle.SetFrameBorderSize(1);
+    HHStyle.SetFrameFillColor(0);
+    HHStyle.SetFrameFillStyle(0);
+    HHStyle.SetFrameLineColor(1);
+    HHStyle.SetFrameLineStyle(1);
+    HHStyle.SetFrameLineWidth(1);
+            
+    // ==============
+    //  Histo
+    // ==============
+
+    HHStyle.SetErrorX(0.0);
+    HHStyle.SetEndErrorSize(0);
+            
+    // HHStyle.SetHistFillColor(1);
+    // HHStyle.SetHistFillStyle(0);
+    // HHStyle.SetHistLineColor(1);
+    HHStyle.SetHistLineStyle(0);
+    HHStyle.SetHistLineWidth(1);
+    // HHStyle.SetLegoInnerR(Float_t rad = 0.5);
+    // HHStyle.SetNumberContours(Int_t number = 20);
+
+    // HHStyle.SetErrorMarker(20);
+            
+    HHStyle.SetMarkerStyle(20);
+            
+    // ==============
+    //  Fit/function
+    // ==============
+            
+    HHStyle.SetOptFit(1);
+    HHStyle.SetFitFormat("5.4g");
+    HHStyle.SetFuncColor(2);
+    HHStyle.SetFuncStyle(1);
+    HHStyle.SetFuncWidth(1);
+            
+    // ==============
+    //  Date
+    // ============== 
+            
+    HHStyle.SetOptDate(0);
+    // HHStyle.SetDateX(Float_t x = 0.01);
+    // HHStyle.SetDateY(Float_t y = 0.01);
+            
+    // =====================
+    //  Statistics Box
+    // =====================
+            
+    HHStyle.SetOptFile(0);
+    HHStyle.SetOptStat(0); // To display the mean and RMS:   SetOptStat("mr");
+    HHStyle.SetStatColor(kWhite);
+    HHStyle.SetStatFont(fontstyle);
+    HHStyle.SetStatFontSize(0.025);
+    HHStyle.SetStatTextColor(1);
+    HHStyle.SetStatFormat("6.4g");
+    HHStyle.SetStatBorderSize(1);
+    HHStyle.SetStatH(0.1);
+    HHStyle.SetStatW(0.15);
+    // HHStyle.SetStatStyle(Style_t style = 1001);
+    // HHStyle.SetStatX(Float_t x = 0);
+    // HHStyle.SetStatY(Float_t y = 0);
+            
+    // ==============
+    //  Margins
+    // ==============
+
+    HHStyle.SetPadTopMargin(0.1);
+    HHStyle.SetPadBottomMargin(0.15);
+    HHStyle.SetPadLeftMargin(0.20);
+    HHStyle.SetPadRightMargin(0.05);
+            
+    // ==============
+    //  Global Title
+    // ==============
+            
+    HHStyle.SetOptTitle(0);
+    HHStyle.SetTitleFont(fontstyle);
+    HHStyle.SetTitleColor(1);
+    HHStyle.SetTitleTextColor(1);
+    HHStyle.SetTitleFillColor(10);
+    HHStyle.SetTitleFontSize(0.05);
+    // HHStyle.SetTitleH(0); // Set the height of the title box
+    // HHStyle.SetTitleW(0); // Set the width of the title box
+    // HHStyle.SetTitleX(0); // Set the position of the title box
+    // HHStyle.SetTitleY(0.985); // Set the position of the title box
+    // HHStyle.SetTitleStyle(Style_t style = 1001);
+    // HHStyle.SetTitleBorderSize(2);
+            
+    // ==============
+    //  Axis titles
+    // ==============
+            
+    HHStyle.SetTitleColor(1, "XYZ");
+    HHStyle.SetTitleFont(fontstyle, "XYZ");
+    HHStyle.SetTitleSize(0.04, "XYZ");
+    // HHStyle.SetTitleXSize(Float_t size = 0.02); // Another way to set the size?
+    // HHStyle.SetTitleYSize(Float_t size = 0.02);
+    HHStyle.SetTitleXOffset(1.25);
+    HHStyle.SetTitleYOffset(1.6);
+    // HHStyle.SetTitleOffset(1.1, "Y"); // Another way to set the Offset
+            
+    // ==============
+    //  Axis Label
+    // ==============
+            
+    //HHStyle.SetLabelColor(1, "XYZ");
+    HHStyle.SetLabelFont(fontstyle, "XYZ");
+    HHStyle.SetLabelOffset(0.007, "XYZ");
+    HHStyle.SetLabelSize(0.04, "XYZ");
+            
+    // ==============
+    //  Axis
+    // ==============
+            
+    HHStyle.SetAxisColor(1, "XYZ");
+    HHStyle.SetStripDecimals(kTRUE);
+    HHStyle.SetTickLength(0.03, "XYZ");
+    HHStyle.SetNdivisions(510, "XYZ");
+    HHStyle.SetPadTickX(1);  // To get tick marks on the opposite side of the frame
+    HHStyle.SetPadTickY(1);
+            
+    // Change for log plots:
+    HHStyle.SetOptLogx(0);
+    HHStyle.SetOptLogy(0);
+    HHStyle.SetOptLogz(0);
+            
+    // ==============
+    //  Text
+    // ==============
+            
+    HHStyle.SetTextAlign(11);
+    HHStyle.SetTextAngle(0);
+    HHStyle.SetTextColor(1);
+    HHStyle.SetTextFont(fontstyle);
+    HHStyle.SetTextSize(0.05);
+            
+    // =====================
+    //  Postscript options:
+    // =====================
+            
+    HHStyle.SetPaperSize(20.,20.);
+    // HHStyle.SetLineScalePS(Float_t scale = 3);
+    // HHStyle.SetLineStyleString(Int_t i, const char* text);
+    // HHStyle.SetHeaderPS(const char* header);
+    // HHStyle.SetTitlePS(const char* pstitle);
+            
+    // HHStyle.SetBarOffset(Float_t baroff = 0.5);
+    // HHStyle.SetBarWidth(Float_t barwidth = 0.5);
+    // HHStyle.SetPaintTextFormat(const char* format = "g");
+    // HHStyle.SetPalette(Int_t ncolors = 0, Int_t* colors = 0);
+    // HHStyle.SetTimeOffset(Double_t toffset);
+    // HHStyle.SetHistMinimumZero(kTRUE);
+}
+
+  void DrawDecayChLabel(TString decaychannel, double textSize)
+  {
+    // Draw label for Decay Channel in upper left corner of plot
+
+    TPaveText *decch = new TPaveText();
+
+    decch -> AddText(decaychannel);
+
+    decch -> SetX1NDC(      gStyle->GetPadLeftMargin() + gStyle->GetTickLength()        );
+    decch -> SetY1NDC(1.0 - gStyle->GetPadTopMargin()  - gStyle->GetTickLength() - 0.05 );
+    decch -> SetX2NDC(      gStyle->GetPadLeftMargin() + gStyle->GetTickLength() + 0.15 );
+    decch -> SetY2NDC(1.0 - gStyle->GetPadTopMargin()  - gStyle->GetTickLength()        );
+
+    decch -> SetFillStyle(0);
+    decch -> SetBorderSize(0);
+    if(textSize!=0) decch->SetTextSize(textSize);
+    decch -> SetTextAlign(12);
+    decch -> Draw("same");
+  }
+
+  void DrawCMSLabels(bool cmsprelim, double luminosity, double textSize)
+  {
+    // Draw official labels (CMS Preliminary, luminosity and CM energy) above plot
+
+    TPaveText *label = new TPaveText();
+
+    label -> SetX1NDC(gStyle->GetPadLeftMargin());
+    label -> SetY1NDC(1.0-gStyle->GetPadTopMargin());
+    label -> SetX2NDC(1.0-gStyle->GetPadRightMargin());
+    label -> SetY2NDC(1.0);
+    label -> SetTextFont(42);
+
+    if (cmsprelim)
+      {
+        label -> AddText(Form("CMS Preliminary, %2.1f fb^{-1} at #sqrt{s} = 7 TeV",luminosity/1000));
+      }
+    else
+      {
+        label -> AddText(Form("CMS, %2.1f fb^{-1} at #sqrt{s} = 7 TeV",luminosity/1000));
+      }
+
+    label->SetFillStyle(0);
+    label->SetBorderSize(0);
+    if(textSize!=0) label->SetTextSize(textSize);
+    label->SetTextAlign(32);
+    label->Draw("same");
+  }
+
+
+///////////////////////////////////////////
+
+TObject *RootFileReader::GetObj(const char * filename, const char * histoname, bool allowNonexisting) {
+    auto file = fileMap[filename];
+    if (!file) {
+        file = TFile::Open(filename);
+        if (!file) {
+            if (allowNonexisting) return nullptr;
+            std::cerr << "The file " << filename << " does not exist, thus cannot get histogram " << histoname << std::endl;
+            exit(1);
+        }
+        fileOrder.push_back(filename);
+        ++opened[filename];
+    }
+    ++accessed[filename];
+    //at max, keep 20 files open
+    if (fileMap.size() > 20) {
+        //delete 0th element
+        delete fileMap[fileOrder[0]];
+        fileMap.erase(fileOrder[0]);
+        fileOrder.erase(fileOrder.begin());
+    }
+    return file->Get(histoname);
+}
+
+template<typename T>
+void RootFileReader::Get(const char * filename, const char * histoname, T*& result, bool allowNonexisting) {
+    result = dynamic_cast<T*>(GetObj(filename, histoname, allowNonexisting));
+    if (!result && !allowNonexisting) {
+        std::cerr << "The histogram " << histoname << " in file " << filename 
+            << " is of incompatible type (cannot typecast)!" << std::endl;
+        exit(1);
+    }
+}
+
+template <typename T>
+T RootFileReader::Get(const char* filename, const char* histoname, bool allowNonexisting) {
+    static_assert(std::is_pointer<T>::value == true, "You must convert to a pointer type!");
+    T result;
+    Get(filename, histoname, result, allowNonexisting);
+    return result;
+}
+
+    
+RootFileReader::~RootFileReader()
+{
+    std::cout << "Deleting RootFileReader\n";
+    for (const auto& i : fileMap) {
+        delete i.second;
+    }
+    for (const auto& i : accessed) {
+        std::cout << "accessed: " << i.first << " " << i.second << std::endl;
+    }
+    for (const auto& i : opened) {
+        std::cout << "opened: " << i.first << " " << i.second << std::endl;
+    }
+    
+}
+
+RootFileReader* RootFileReader::getInstance() {
+    static RootFileReader instance;
+    return &instance;
 }
--- Configuration/analysis/diLeptonic/utils.h
+++ Configuration/analysis/diLeptonic/utils.h
@@ -2,11 +2,40 @@
 #define util_h
 
 #include "classes.h"
+#include <map>
+#include <TString.h>
+#include <TFile.h>
+#include <TStyle.h>
 
 //convert LorentzVector to an array[E, px, py, pz]
 void LVtod4(const LV lv, double *d);
 
 // convert double to string (smart number of digits)
 std::string d2s(double d);
+
+void setHHStyle(TStyle& HHStyle);
+void DrawDecayChLabel(TString decaychannel="", double textSize=0.04);
+void DrawCMSLabels(bool cmsprelim=true, double luminosity=0.0, double textSize=0.04);
+
+class RootFileReader {
+    std::map<TString, TFile*> fileMap;
+    std::vector<TString> fileOrder;
+    std::map<TString, int> accessed, opened;
+    
+    TObject* GetObj(const char* filename, const char* histoname, bool allowNonexisting);
+    RootFileReader() {};
+    ~RootFileReader();
+public:
+    //returns the singleton instnce
+    static RootFileReader* getInstance();
+    
+    //get a histogram from the file
+    template <typename T>
+    void Get(const char* filename, const char* histoname, T*& result, bool allowNonexisting = false);
+    
+    //get a histogram from the file, you need to pass the type here
+    template <typename T>
+    T Get(const char* filename, const char* histoname, bool allowNonexisting = false);
+};
 
 #endif

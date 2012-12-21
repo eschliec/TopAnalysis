b4caa734393f8e1d28f8d5f3717c529a6f75b70b
tdorland <>
2012-12-21 19:23:41 +0000

    Update to new table style

--- Configuration/analysis/diLeptonic/mk_resultstable.sh
+++ Configuration/analysis/diLeptonic/mk_resultstable.sh
@@ -2,58 +2,104 @@
 echo "\\\begin{table}[h]"
 echo "	\\\begin{center}"
 echo "		\\\caption{\\\small"
-echo "		Normalised differential cross section as a function of lepton observables $ dp_T^{\\\ell}$, $\\\eta^{\\\ell}$, the transverse momentum of the lepton pair, $ p_T^{\\\ell\\\ell}$, and the invariant mass of the lepton pair $ m^{\\\ell\\\ell}$.}"
-echo "		\\\label{tab:diffxseclep}"
+echo "		 Normalised differential cross section as a function of the \\\pt-ordered lepton observables: the transverse momentum ($ p_T^{Lead.l}$, $p_T^{NLead.l}$) and the pseudorapidity ($\\\eta^{Lead.l}$, $\\\eta^{NLead.l}$).}"
+echo "		\\\label{tab:diffxsecorderedlepton}"
 echo "		\\\begin{tabular}{c|c||c|c|c|c}" 
 echo "			\\\hline "
-echo "			\\\hline            bin-center [GeV]     & bin [GeV] & $ 1/\\\sigma d\\\sigma/dp_T^{\\\ell}$ & stat. [\\\%] & sys. [\\\%] & total [\\\%] \\\\\\"
+echo "			\\\hline            bin-center [GeV]     & bin [GeV] & $ 1/\\\sigma d\\\sigma/dp_T^{Lead.l}$ & stat. [\\\%] & sys. [\\\%] & total [\\\%] \\\\\\"
 echo "		\\\hline"
-tail -n +2  combined/LeptonpTResultsLaTeX.txt
+tail -n +2  combined/LeptonpTLeadResultsLaTeXAfter.txt
+echo "			\\\hline "
+echo "			\\\hline            bin-center [GeV]     & bin [GeV] & $ 1/\\\sigma d\\\sigma/dp_T^{NLead.l}$ & stat. [\\\%] & sys. [\\\%] & total [\\\%] \\\\\\"
+echo "		\\\hline"
+tail -n +2  combined/LeptonpTNLeadResultsLaTeXAfter.txt
 echo "			\\\hline"
-echo "			\\\hline            bin-center       & bin  & $ 1/\\\sigma d\\\sigma/d\\\eta^{\\\ell}$ & stat. [\\\%] & sys. [\\\%] & total [\\\%] \\\\\\"
+echo "			\\\hline            bin-center       & bin  & $ 1/\\\sigma d\\\sigma/d\\\eta^{Lead.l}$ & stat. [\\\%] & sys. [\\\%] & total [\\\%] \\\\\\"
 echo "			\\\hline"
-tail -n +2 combined/LeptonEtaResultsLaTeX.txt
+tail -n +2 combined/LeptonEtaLeadResultsLaTeXAfter.txt
+echo "			\\\hline"
+echo "			\\\hline            bin-center       & bin  & $ 1/\\\sigma d\\\sigma/d\\\eta^{NLead.l}$ & stat. [\\\%] & sys. [\\\%] & total [\\\%] \\\\\\"
+echo "			\\\hline"
+tail -n +2 combined/LeptonEtaNLeadResultsLaTeXAfter.txt
 echo "			\\\hline"
 echo "			\\\hline            bin-center [GeV]      & bin [GeV] & $ 1/\\\sigma d\\\sigma/dp_T^{\\\ell\\\ell}$ & stat. [\\\%] & sys. [\\\%] & total [\\\%] \\\\\\"
 echo "			\\\hline "
-tail -n +2 combined/LLBarpTResultsLaTeX.txt
+tail -n +2 combined/LLBarpTResultsLaTeXAfter.txt
 echo "			\\\hline"
 echo "			\\\hline            bin-center [GeV]      & bin [GeV] & $ 1/\\\sigma d\\\sigma/dm^{\\\ell\\\ell}$ & stat. [\\\%] & sys. [\\\%] & total [\\\%] \\\\\\"
 echo "			\\\hline"
-tail -n +2 combined/LLBarMassResultsLaTeX.txt
+tail -n +2 combined/LLBarMassResultsLaTeXAfter.txt
 echo "		\\\hline"
 echo "  	\\\hline"
 echo "		\\\end{tabular}"
 echo "	\\\end{center}"
 echo "\\\end{table}"
-
+echo " "
+echo "\\\begin{table}[h]"
+echo "	\\\begin{center}"
+echo "		\\\caption{\\\small"
+echo "		 Normalised differential cross section as a function of lepton observables: the transverse momentum of the lepton pair $ p_T^{\\\ell\\\ell}$, and the invariant mass of the lepton pair $ m^{\\\ell\\\ell}$.}"
+echo "		\\\label{tab:diffxseclep}"
+echo "		\\\begin{tabular}{c|c||c|c|c|c}" 
+echo "			\\\hline"
+echo "			\\\hline            bin-center [GeV]      & bin [GeV] & $ 1/\\\sigma d\\\sigma/dp_T^{\\\ell\\\ell}$ & stat. [\\\%] & sys. [\\\%] & total [\\\%] \\\\\\"
+echo "			\\\hline "
+tail -n +2 combined/LLBarpTResultsLaTeXAfter.txt
+echo "			\\\hline"
+echo "			\\\hline            bin-center [GeV]      & bin [GeV] & $ 1/\\\sigma d\\\sigma/dm^{\\\ell\\\ell}$ & stat. [\\\%] & sys. [\\\%] & total [\\\%] \\\\\\"
+echo "			\\\hline"
+tail -n +2 combined/LLBarMassResultsLaTeXAfter.txt
+echo "		\\\hline"
+echo "  	\\\hline"
+echo "		\\\end{tabular}"
+echo "	\\\end{center}"
+echo "\\\end{table}"
 echo " "
 echo "\\\begin{table}[h]"
 echo "	\\\begin{center}"
 echo "		\\\caption{\\\small "
-echo "		Normalised differential cross section as a function of top quark observables, the transverse momentum of the top quarks, $ p_T^{t~{\\\rm or}~\\\bar{t}}$, the rapidity of the top quarks , $ y^{t~{\\\rm or}~\\\bar{t}}$, the transverse momentum of the top quark pair, $ p_T^{\\\ttbar}$ and the invariant mass of the quark pair $ m^{\\\ttbar}$.}"
+echo "		Normalised differential cross section as a function of the \\\pt-ordered top-quark observables: the transverse momentum ($ p_T^{Lead.t}$, $p_T^{NLead.t}$) and the rapidity ($y^{Lead.t}$, $y^{NLead.t}$).}"
+echo "		\\\label{tab:diffxsecorederedtop}"
+echo "		\\\begin{tabular}{c|c||c|c|c|c} "
+echo "			\\\hline "
+echo "			\\\hline            bin-center [GeV]      & bin [GeV] & $ 1/\\\sigma d\\\sigma/p_T^{t~{Lead.t}$ & stat. [\\\%] & sys. [\\\%] & total [\\\%] \\\\\\"
+echo "		\\\hline"
+tail -n +2 combined/ToppTLeadResultsLaTeXAfter.txt
+echo "			\\\hline"
+echo "			\\\hline            bin-center       & bin & $ 1/\\\sigma d\\\sigma/p_T^{t~{NLead.t}$ & stat. [\\\%] & sys. [\\\%] & total [\\\%] \\\\\\"
+echo "			\\\hline"
+tail -n +2 combined/ToppTNLeadResultsLaTeXAfter.txt
+echo "			\\\hline"
+echo "			\\\hline            bin-center       & bin & $ 1/\\\sigma d\\\sigma/y^{t~{Lead.t}$ & stat. [\\\%] & sys. [\\\%] & total [\\\%] \\\\\\"
+echo "			\\\hline"
+tail -n +2 combined/TopRapidityLeadResultsLaTeXAfter.txt
+echo "			\\\hline"
+echo "			\\\hline            bin-center       & bin & $ 1/\\\sigma d\\\sigma/y^{t~{NLead.t}$ & stat. [\\\%] & sys. [\\\%] & total [\\\%] \\\\\\"
+echo "			\\\hline"
+tail -n +2 combined/TopRapidityNLeadResultsLaTeXAfter.txt
+echo "			\\\hline"
+echo "		\\\end{tabular}"
+echo "	\\\end{center}"
+echo "\\\end{table}"
+echo " "
+echo "\\\begin{table}[h]"
+echo "	\\\begin{center}"
+echo "		\\\caption{\\\small "
+echo "		Normalised differential cross section as a function of top quark observables, the transverse momentum of the top quarks, $ p_T^{t}$, the rapidity of the top quarks , $ y^{t}$, the transverse momentum of the top quark pair, $ p_T^{\ttbar}$ and the invariant mass of the quark pair $ m^{\ttbar}$.}"
 echo "		\\\label{tab:diffxsectop}"
 echo "		\\\begin{tabular}{c|c||c|c|c|c} "
 echo "			\\\hline "
-echo "			\\\hline            bin-center [GeV]      & bin [GeV] & $ 1/\\\sigma d\\\sigma/p_T^{t~{\\\rm or}~\\\bar{t}}$ & stat. [\\\%] & sys. [\\\%] & total [\\\%] \\\\\\"
-echo "		\\\hline"
-tail -n +2 combined/ToppTResultsLaTeX.txt
-echo "			\\\hline"
-echo "			\\\hline            bin-center       & bin & $ 1/\\\sigma d\\\sigma/y^{t~{\\\rm or}~\\\bar{t}}$ & stat. [\\\%] & sys. [\\\%] & total [\\\%] \\\\\\"
-echo "			\\\hline"
-tail -n +2 combined/TopRapidityResultsLaTeX.txt
-echo "			\\\hline"
 echo "			\\\hline            bin-center [GeV]      & bin [GeV] & $ 1/\\\sigma d\\\sigma/d p_T^{\\\ttbar}$ & stat. [\\\%] & sys. [\\\%] & total [\\\%] \\\\\\"
 echo "			\\\hline "
-tail -n +2 combined/TTBarpTResultsLaTeX.txt
+tail -n +2 combined/TTBarpTResultsLaTeXAfter.txt
 echo "			\\\hline" 
 echo "			\\\hline            bin-center       & bin & $ 1/\\\sigma d\\\sigma/y^{\\\ttbar}$ & stat. [\\\%] & sys. [\\\%] & total [\\\%] \\\\\\"
 echo "			\\\hline"
-tail -n +2 combined/TTBarRapidityResultsLaTeX.txt
+tail -n +2 combined/TTBarRapidityResultsLaTeXAfter.txt
 echo "			\\\hline" 
 echo "			\\\hline            bin-center [GeV]      & bin [GeV] & $ 1/\\\sigma d\\\sigma/dm^{\\\ttbar}$ & stat. [\\\%] & sys. [\\\%] & total [\\\%] \\\\\\"
 echo "			\\\hline"		
-tail -n +2 combined/TTBarMassResultsLaTeX.txt
+tail -n +2 combined/TTBarMassResultsLaTeXAfter.txt
 echo "                  \\\hline"
 echo "  		\\\hline"
 echo "		\\\end{tabular}"
@@ -63,17 +109,25 @@ echo " "
 echo "\\\begin{table}[h]"
 echo "	\\\begin{center}"
 echo "		\\\caption{\\\small"
-echo "                  Normalised differential cross section as a function of b-jet observables, the transverse momentum of the b-jets, $ p_T^{b~{\\\rm or}~\\\bar{b}}$, the pseudorapidity of the b-jets, $\\\eta^{b~{\\\rm or}~\\\bar{b}}$.}"
-echo "		\\\label{tab:diffxsecbjet}"
+echo "                  Normalised differential cross section as a function of the \\\pt-ordered bjet observables: the transverse momentum ($ p_T^{Lead.b-jet}$, $p_T^{NLead.b-jet}$) and the pseudorapidity ($\\\eta^{Lead.b-jet}$, $\\\eta^{NLead.b-jet}$).}"
+echo "		\\\label{tab:diffxsecorderedbjet}"
 echo "		\\\begin{tabular}{c|c||c|c|c|c}"
 echo "			\\\hline" 
-echo "			\\\hline            bin-center [GeV]      & bin [GeV] & $ 1/\\\sigma d\\\sigma/p_T^{b~{\\\rm or}~\\\bar{b}}$ & stat. [\\\%] & sys. [\\\%] & total [\\\%] \\\\\\"
+echo "			\\\hline            bin-center [GeV]      & bin [GeV] & $ 1/\\\sigma d\\\sigma/p_T^{b~{Lead.b-jet}$ & stat. [\\\%] & sys. [\\\%] & total [\\\%] \\\\\\"
 echo "		\\\hline"
-tail -n +2 combined/BJetpTResultsLaTeX.txt
-echo "			\\\hline"
-echo "			\\\hline            bin-center       & bin & $ 1/\\\sigma d\\\sigma/\\\eta^{b~{\\\rm or}~\\\bar{b}}$ & stat. [\\\%] & sys. [\\\%] & total [\\\%] \\\\\\"
+tail -n +2 combined/BJetpTLeadResultsLaTeXAfter.txt
 echo "			\\\hline" 
-tail -n +2 combined/BJetEtaResultsLaTeX.txt
+echo "			\\\hline            bin-center [GeV]      & bin [GeV] & $ 1/\\\sigma d\\\sigma/p_T^{b~{NLead.b-jet}$ & stat. [\\\%] & sys. [\\\%] & total [\\\%] \\\\\\"
+echo "		\\\hline"
+tail -n +2 combined/BJetpTNLeadResultsLaTeXAfter.txt
+echo "			\\\hline"
+echo "			\\\hline            bin-center       & bin & $ 1/\\\sigma d\\\sigma/\\\eta^{Lead.b-jet}$ & stat. [\\\%] & sys. [\\\%] & total [\\\%] \\\\\\"
+echo "			\\\hline" 
+tail -n +2 combined/BJetEtaLeadResultsLaTeXAfter.txt
+echo "			\\\hline"
+echo "			\\\hline            bin-center       & bin & $ 1/\\\sigma d\\\sigma/\\\eta^{NLead.b-jet}$ & stat. [\\\%] & sys. [\\\%] & total [\\\%] \\\\\\"
+echo "			\\\hline" 
+tail -n +2 combined/BJetEtaNLeadResultsLaTeXAfter.txt
 echo "  		\\\hline"
 echo "		\\\end{tabular}"
 echo "	\\\end{center}"

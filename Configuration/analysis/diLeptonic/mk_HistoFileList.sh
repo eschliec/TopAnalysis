3851adad3e6c556f17dbe3e6604b24e9cea39c31
iasincru <>
2013-01-25 15:19:18 +0000

    Added extra BTAG (LJET_PT, LJET_ETA, LJET) and efficiencies (BEFF, CEFF, LEFF) variations

--- Configuration/analysis/diLeptonic/mk_HistoFileList.sh
+++ Configuration/analysis/diLeptonic/mk_HistoFileList.sh
@@ -8,8 +8,12 @@ foreach sample (run qcd dyee dymumu dytautau ww wz zz wtolnu single ttbarbg ttba
    foreach channel (ee emu mumu)
      
        
-       foreach Syst  (Nominal JESUP JESDOWN JERUP JERDOWN PU_UP PU_DOWN SCALEUP SCALEDOWN MATCHUP MATCHDOWN MASSUP MASSDOWN POWHEG MCATNLO HADUP HADDOWN BTAG_UP BTAG_DOWN BTAG_PT_UP BTAG_PT_DOWN BTAG_ETA_UP BTAG_ETA_DOWN TRIG_UP TRIG_DOWN)
-      
+      foreach Syst  (Nominal JESUP JESDOWN JERUP JERDOWN PU_UP PU_DOWN TRIG_UP TRIG_DOWN \
+                     SCALEUP SCALEDOWN MATCHUP MATCHDOWN MASSUP MASSDOWN POWHEG MCATNLO HADUP HADDOWN \
+                     BTAG_UP BTAG_DOWN BTAG_LJET_UP BTAG_LJET_DOWN \
+                     BTAG_PT_UP BTAG_PT_DOWN BTAG_ETA_UP BTAG_ETA_DOWN \
+                     BTAG_LJET_PT_UP BTAG_LJET_PT_DOWN BTAG_LJET_ETA_UP BTAG_LJET_ETA_DOWN \
+                     BTAG_BEFF_UP BTAG_BEFF_DOWN BTAG_CEFF_UP BTAG_CEFF_DOWN BTAG_LEFF_UP BTAG_LEFF_DOWN)
 
       ls -1 selectionRoot/$Syst/$channel/${channel}_$sample*.root >> FileLists/HistoFileList_$Syst\_$channel.txt
       ls -1 selectionRoot/$Syst/$channel/${channel}_$sample*.root >> FileLists/HistoFileList_$Syst\_combined.txt

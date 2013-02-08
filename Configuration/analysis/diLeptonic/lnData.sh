c40e49086fd8244c266f3372d93c63d2eace9b85
iasincru <>
2013-02-08 11:10:23 +0000

    Added also the KinFit variations

--- Configuration/analysis/diLeptonic/lnData.sh
+++ Configuration/analysis/diLeptonic/lnData.sh
@@ -6,10 +6,12 @@ foreach channel (ee emu mumu)
    
    foreach syst (JES_DOWN JES_UP JER_DOWN JER_UP MATCH_UP MATCH_DOWN MASS_UP MASS_DOWN PU_UP PU_DOWN TRIG_UP TRIG_DOWN SCALE_UP SCALE_DOWN MCATNLO POWHEG SPINCORR \
                  LEPT_UP LEPT_DOWN \
+                 KIN_UP KIN_DOWN \
                  BTAG_UP BTAG_DOWN BTAG_LJET_UP BTAG_LJET_DOWN \
                  BTAG_PT_UP BTAG_PT_DOWN BTAG_ETA_UP BTAG_ETA_DOWN \
                  BTAG_LJET_PT_UP BTAG_LJET_PT_DOWN BTAG_LJET_ETA_UP BTAG_LJET_ETA_DOWN \
-                 BTAG_BEFF_UP BTAG_BEFF_DOWN BTAG_CEFF_UP BTAG_CEFF_DOWN BTAG_LEFF_UP BTAG_LEFF_DOWN)
+                 BTAG_BEFF_UP BTAG_BEFF_DOWN BTAG_CEFF_UP BTAG_CEFF_DOWN BTAG_LEFF_UP BTAG_LEFF_DOWN
+        )
         if [ -d selectionRoot/$syst/$channel ] ; then
             cd selectionRoot/$syst/$channel/
             echo
--- Configuration/analysis/diLeptonic/mk_HistoFileList.sh
+++ Configuration/analysis/diLeptonic/mk_HistoFileList.sh
@@ -10,6 +10,7 @@ foreach sample (run qcd dyee dymumu dytautau ww wz zz wtolnu single ttbarbg ttba
       foreach Syst  (Nominal \
                      PU_UP PU_DOWN TRIG_UP TRIG_DOWN \
                      LEPT_UP LEPT_DOWN \
+                     KIN_UP KIN_DOWN \
                      SCALE_UP SCALE_DOWN MATCH_UP MATCH_DOWN MASS_UP MASS_DOWN POWHEG MCATNLO HAD_UP HAD_DOWN \
                      BTAG_UP BTAG_DOWN BTAG_LJET_UP BTAG_LJET_DOWN \
                      BTAG_PT_UP BTAG_PT_DOWN BTAG_ETA_UP BTAG_ETA_DOWN \

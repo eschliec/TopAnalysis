3c53a05dbedc5373e889b22b8636f6a4a3eca9a3
wbehrenh <>
2013-02-19 09:29:43 +0000

    added some output

--- Configuration/analysis/diLeptonic/compare_free_fixed.C
+++ Configuration/analysis/diLeptonic/compare_free_fixed.C
@@ -2,6 +2,13 @@
 #include <TFile.h>
 #include <TLegend.h>
 #include <TCanvas.h>
+#include <TGraphAsymmErrors.h>
+#include <iomanip>
+
+//run these commands to compare two results:
+//rm compare_free_fixed.txt
+//for i in `awk '{print $1}'<HistoList`;do root -b -q compare_free_fixed.C'("'$i'")' >>| compare_free_fixed.txt;done
+
 void compare_free_fixed(TString quantity) {
 
 //     TFile ffixed("Plots_kinSF_173/combined/DiffXS_"+quantity+"_source.root");
@@ -19,6 +26,7 @@ void compare_free_fixed(TString quantity) {
     data_staterror_only->SetLineWidth(2);
     data_staterror_only->SetLineColor(kBlue);
     data_staterror_only->SetFillColor(kWhite);
+    TGraphAsymmErrors *fixed_data = data_staterror_only->Clone();
     
     data_staterror_only->Draw("p,same");
     l.AddEntry(data_staterror_only, "m_t = 173 GeV");
@@ -32,8 +40,11 @@ void compare_free_fixed(TString quantity) {
     diff -= x;
     
     for (int i = 0; i < data_staterror_only->GetN(); ++i) {
+        double fixed_y;
+        fixed_data->GetPoint(i,x,fixed_y);
         data_staterror_only->GetPoint(i,x,y);
         data_staterror_only->SetPoint(i,x+diff/6,y);
+        cout << quantity << " " << i << " " << std::setprecision(2) << 100*(1-y/fixed_y) << " %\n";
     }
     
     

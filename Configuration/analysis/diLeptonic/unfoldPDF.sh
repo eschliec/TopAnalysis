0e7cfbb88acdfff0045cb1dac18d9e031ea02b40
wbehrenh <>
2013-02-21 14:34:06 +0000

    run ./mk_HistoFileList.sh before and after PDF stuff

--- Configuration/analysis/diLeptonic/unfoldPDF.sh
+++ Configuration/analysis/diLeptonic/unfoldPDF.sh
@@ -3,6 +3,9 @@
 
 runSpecificVariation() {
     variation="$1"
+    echo "*********************************************************"
+    echo "* starting to unfold variation $variation"
+    echo "*********************************************************"
     for ch in ee emu mumu; do
         rm FileLists/HistoFileList_Nominal_$ch.txt
         cp HistoFileList_Nominal_$ch.txt FileLists/
@@ -10,17 +13,21 @@ runSpecificVariation() {
     done
     mv -f Plots Plots_temp
     # calculate inclusive xsection
+    if [ -d "Plots_temp/$variation" ] ; then mv "Plots_temp/${variation}" Plots ; fi
     mkdir -p Plots/combined
     ./Histo -t cp -p XSec -s Nominal
     # now calculate differential distributions
-    #for plot in HypToppT; do
-        #./Histo -t unfold -p +$plot -s Nominal
-    #done
-    if [ -d "Plots_temp/$variation" ] ; then rm -rf "Plots_temp/${variation}" ; fi
+    #for plot in `awk '{print $1}' HistoList | grep Hyp`; do
+    for plot in HypTTBarMass; do
+        ./Histo -t unfold -p +$plot -s Nominal &
+    done
+    wait
     mv -f Plots "Plots_temp/${variation}"
     mv -f Plots_temp Plots
 }
 
+
+./mk_HistoFileList.sh
 
 for i in ee emu mumu combined; do
     grep -v ttbarsignalplustau.root < FileLists/HistoFileList_Nominal_$i.txt >| HistoFileList_Nominal_$i.txt 
@@ -33,4 +40,8 @@ for no in `seq 1 22`; do
         runSpecificVariation "PDF_${no}_${var}"
     done
 done
+
+./mk_HistoFileList.sh
+
+echo "Done"
 
